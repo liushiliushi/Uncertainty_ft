4f04d1c04314e0c419b11cd436b2691b29b32c39
root <liushiliushi0@gmail.com>
2025-01-06 05:27:11 +0000

    change scripts

--- scripts.sh
+++ scripts.sh
@@ -1,9 +1,8 @@
 
 # Trivia_QA
-unzip /dataset/grade_school_math/data/train_response2.jsonl.zip /dataset/grade_school_math/data
-unzip /dataset/grade_school_math/data/test_response2.jsonl.zip /dataset/grade_school_math/data
-unzip /dataset/trivia_qa/tqa_train_single.jsonl.zip /dataset/trivia_qa
-python uncertainty_sft.py --use_wandb --do_sample False --temperature 0.1 --test_original_model True --on_policy False --use_peft --peft_method lora --model_name /home/jl_fs/meta-llama/Meta-Llama-3.1-8B-Instruct --output_dir checkpoints/trivia --dataset trivia_qa --batch_size_training=8 --val_batch_size=8  --batch_size_testing=10 --generate=llm --lr=5e-5 --loss_type=sot --num_epochs=2
+python generate_response.py --output_dir ../dataset/grade_school_math/data/train_response_temp=0.jsonl --model_name /home/jl_fs/meta-llama/Llama-3.1-8B-Instruct-official --dataset gsm8k_dataset --split train
+python generate_response.py --output_dir ../dataset/grade_school_math/data/test_response_temp=0.jsonl --model_name /home/jl_fs/meta-llama/Llama-3.1-8B-Instruct-official --dataset gsm8k_dataset --split test
+python uncertainty_sft.py --use_wandb --add_loss_con True --on_policy False --batch_size_testing 10 --test_original_model False --do_sample False --temperature 0.1 --use_peft --peft_method lora --model_name /home/jl_fs/meta-llama/Llama-3.1-8B-Instruct-official --output_dir checkpoints/trivia_0104_loss_con --dataset trivia_qa --batch_size_training=8 --val_batch_size=8 --generate=llm --lr=5e-5 --loss_type=sot --num_epochs=2
 # Key parameters:
 # --on_policy = True/False : if the model always generate new answers when training
 # --test_original_model = True/False : If to test the performance of original Llama
