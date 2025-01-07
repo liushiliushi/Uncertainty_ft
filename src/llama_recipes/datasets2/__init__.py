dbe99b5a4f1621cbfea939421a7354e67b39993c
root <liushiliushi0@gmail.com>
2025-01-07 05:24:36 +0000

    change

--- src/llama_recipes/datasets2/__init__.py
+++ src/llama_recipes/datasets2/__init__.py
@@ -13,7 +13,7 @@ from llama_recipes.datasets2.professional_law import get_professional_law
 from llama_recipes.datasets2.strategyqa_dataset import get_strategyqa
 from llama_recipes.datasets2.object_cou import get_object_cou2
 from llama_recipes.datasets2.trivia_qa import get_trivia_qa, get_trivia_qa_raw
-
+from llama_recipes.datasets2.truthful_qa import get_truthful_qa, get_truthful_qa_raw
 DATASET_PREPROC = {
     "alpaca_dataset": partial(get_alpaca_dataset),
     "grammar_dataset": get_grammar_dataset,
