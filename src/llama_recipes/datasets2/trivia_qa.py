3de3eeca1c36864b6a628a0d0658cafd80c58635
Your Name <you@example.com>
2025-07-27 15:54:08 +0000

    change

--- src/llama_recipes/datasets2/trivia_qa.py
+++ src/llama_recipes/datasets2/trivia_qa.py
@@ -398,7 +398,7 @@ def get_trivia_qa(tokenizer, split, train_config, on_policy = False):
         if train_config.train_gpt:
             path = "../dataset/trivia_qa/validation_gpt_temp=0_1000.jsonl"
         else:
-            path = "../dataset/trivia_qa/validation_response_temp=0.jsonl"
+            path = "../dataset/trivia_qa/validation_response_temp=0_10000.jsonl"
         dataset = datasets.load_dataset('json', data_files=path, split='train[:1000]')
     else:
         path = "../dataset/trivia_qa/validation_response_temp=0_10000.jsonl"
